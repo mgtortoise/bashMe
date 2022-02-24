88e4f4cbd383db5226379b8acd60c72b8eb91329
mgtortoise <75561364+mgtortoise@users.noreply.github.com>
2022-02-24 20:52:10 +0630

    Add files via upload

--- hello1.sh
+++ hello1.sh
@@ -10,9 +10,9 @@ cd badvpn-master
 mkdir build
 cd build
 cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_TUN2SOCKS=1 -DBUILD_UDPGW=1;make install;
-cat /etc/default/dropbear
-cd  /etc/default/dropbear
-printf"%s\n" "Dropbear Installed"
+wget https://raw.githubusercontent.com/mgtortoise/bashMe/main/dropbear
+rm -rf /etc/default/dropbear
+mv dropbear /etc/default/dropbear
 /etc/init.d/dropbear restart
 screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
 netstat -tulpn
