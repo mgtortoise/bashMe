96cb49e3df4086d3a4feab6e7900a144cd3ce9af
mgtortoise <75561364+mgtortoise@users.noreply.github.com>
2022-02-24 11:57:56 +0630

    Add files via upload

--- hello1.sh
+++ hello1.sh
@@ -1,5 +1,15 @@
 printf "%s\n" "Started SSH installer"
-apt install dropbear -y;apt install cmake -y;apt install unzip -y;apt install screen -y;apt install wget -y;wget https://github.com/ambrop72/badvpn/archive/master.zip;unzip master.zip;cd badvpn-master;mkdir build;cd build;cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_TUN2SOCKS=1 -DBUILD_UDPGW=1;make install;
+apt install dropbear -y
+apt install cmake -y
+apt install unzip -y
+apt install screen -y
+apt install wget -y
+wget https://github.com/ambrop72/badvpn/archive/master.zip
+unzip master.zip
+cd badvpn-master
+mkdir build
+cd build
+cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_TUN2SOCKS=1 -DBUILD_UDPGW=1;make install;
 cat /etc/default/dropbear
 cd  /etc/default/dropbear
 printf"%s\n" "Dropbear Installed"
