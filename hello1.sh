b4958e233b531d1a15e5e3ce345d3f6c948d96bf
mgtortoise <75561364+mgtortoise@users.noreply.github.com>
2022-06-15 22:08:11 +0630

    Add files via upload

--- hello1.sh
+++ hello1.sh
@@ -4,7 +4,7 @@ apt install cmake -y
 apt install unzip -y
 apt install screen -y
 apt install wget -y
-apt install python -y
+apt install squid -y
 wget https://github.com/ambrop72/badvpn/archive/master.zip
 unzip master.zip
 cd badvpn-master
@@ -12,16 +12,14 @@ mkdir build
 cd build
 cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_TUN2SOCKS=1 -DBUILD_UDPGW=1;make install;
 wget https://raw.githubusercontent.com/mgtortoise/bashMe/main/dropbear
+wget https://raw.githubusercontent.com/mgtortoise/bashMe/main/squid.conf
 rm -rf /etc/default/dropbear
 mv dropbear /etc/default/dropbear
 /etc/init.d/dropbear restart
+rm -rf /etc/squid/squid.conf
+mv squid.conf /etc/squid/squid.conf
+systemctl restart squid
 screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
-cd /etc/systemd/system
-wget https://raw.githubusercontent.com/mgtortoise/pdme/main/main.py
-wget https://raw.githubusercontent.com/mgtortoise/pdme/main/websocket.service
-cd $HOME
-sudo systemctl daemon-reload
-systemctl start websocket
 netstat -tulpn 
 printf "Username = "
 read username 
