1f040763e13bcf39b2b7e9b159765ee9cc322971
mgtortoise <75561364+mgtortoise@users.noreply.github.com>
2022-06-23 23:40:42 +0630

    Add files via upload

--- hello1.sh
+++ hello1.sh
@@ -5,6 +5,7 @@ apt install unzip -y
 apt install screen -y
 apt install wget -y
 apt install squid -y
+apt install shadowsocks-libev -y
 wget https://github.com/ambrop72/badvpn/archive/master.zip
 unzip master.zip
 cd badvpn-master
@@ -13,12 +14,17 @@ cd build
 cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_TUN2SOCKS=1 -DBUILD_UDPGW=1;make install;
 wget https://raw.githubusercontent.com/mgtortoise/bashMe/main/dropbear
 wget https://raw.githubusercontent.com/mgtortoise/bashMe/main/squid.conf
+wget https://raw.githubusercontent.com/mgtortoise/hellogithub/master/config.json
 rm -rf /etc/default/dropbear
 mv dropbear /etc/default/dropbear
 /etc/init.d/dropbear restart
 rm -rf /etc/squid/squid.conf
 mv squid.conf /etc/squid/squid.conf
-service squid restart
+rm -rf /etc/shadowsocks-libev/config.json
+mv config.json /etc/shadowsocks-libev/config.json
+systemctl restart shadowsocks-libev
+systemctl enable shadowsocks-libev
+systemctl restart squid
 screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
 netstat -tulpn 
 printf "Username = "
